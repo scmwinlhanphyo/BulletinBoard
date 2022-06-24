2b5f6ab0b132ac3a9e279f46fa560cfd2b1dd071
scmwinlhanphyo <scm.winlhanphyo@gmail.com>
2022-06-24 10:45:32 +0630

    [feature/post_detail_design] fixed error.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,7 +25,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FooterComponent,
     UserListComponent,
     PostDeleteDialogComponent,
-    PostDetailDialogComponent
+    PostDetailDialogComponent,
     UploadPostComponent
   ],
   imports: [
